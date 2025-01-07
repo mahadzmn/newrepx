66a9fdb7e33534b738f197a666d1fa225c28c53d
mahadzmn <mahadzaman05@gmail.com>
2025-01-08 03:29:04 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://pinkpetals.pk/collections/new-arrivals/products/amber-gold?variant=46468169203935";
-    const blackPageURL = "https://cggkidswear.myfunnelish.com/super-active-toddler-jacket";
+    const whitePageURL = "https://www.takealot.com/plain-t-shirt-white-140g/PLID91375103?srsltid=AfmBOopgbGJ6t-98ecNxDNTWdPigpB8H0PEELs1DorvIwKXAq99dh1kNhttps://www.takealot.com/plain-t-shirt-white-140g/PLID91375103?srsltid=AfmBOopgbGJ6t-98ecNxDNTWdPigpB8H0PEELs1DorvIwKXAq99dh1kN";
+    const blackPageURL = "https://dnfiwbzxjq.myfunnelish.com/imbassd-1736288289182579";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
